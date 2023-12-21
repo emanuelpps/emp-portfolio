959e2e5466bd2a7c0e9005e145fceddebe49de7d
emanuelpps <emanuelpages.ps@gmail.com>
2023-12-20 23:17:50 -0300

    added project page

--- app/work/[projectName]/page.jsx
+++ app/work/[projectName]/page.jsx
@@ -21,18 +21,60 @@ function ProjectName({ params }) {
   console.log(projectPage);
 
   return (
-    <div>
+    <div className="pt-16 grid grid-cols-auto grid-rows-auto gap-4">
       {projectPage ? (
         <>
-          <h1>{projectPage.name}</h1>
-          <h2>{projectPage.class}</h2>
-          <h2>{projectPage.type}</h2>
-          <Image src={projectPage.img} width={1200} height={1200}/>
-          <p>{projectPage.description}</p>
+          <div id="project-title-container" className="col-span-5">
+            <h1 className="text-center text-3xl">{projectPage.name}</h1>
+            <div id="h2-container" className="flex flex-row justify-center">
+              <h2 className="text-center ">{projectPage.class}</h2>
+            </div>
+          </div>
+          <div className="row-span-6 col-start-1 row-start-2"></div>
+          <div
+            id="image-container"
+            className="flex col-span-3 row-span-4 col-start-2 row-start-2"
+          >
+            <Image
+              src={projectPage.img}
+              width={1200}
+              height={1200}
+              className="self-auto"
+              alt="Project Image"
+            />
+          </div>
+          <div
+            id="overview-container"
+            className="grid grid-cols-5 grid-rows-5 gap-4"
+          >
+            <div className="col-span-2 row-span-2">
+              <h4 className="flex flex-row">My Role -</h4>
+              <h3 className="flex flex-row">
+                {projectPage.infoProject[0].myRole}
+              </h3>
+            </div>
+            <div className="col-span-2 row-span-2 col-start-1 row-start-4">
+              <h4>Team</h4>
+              <p>{projectPage.infoProject[0].team}</p>
+            </div>
+            <div className="col-span-2 row-span-5 col-start-4 row-start-1">
+              5
+            </div>
+            <div className="col-span-2 col-start-1 row-start-3">6</div>
+            <div className="row-span-5 col-start-3 row-start-1">7</div>
+          </div>
         </>
       ) : (
         <p className="text-center">Loading Project... Please Wait</p>
       )}
+      <div className="row-span-4 col-start-5 row-start-2">
+        <ul>
+          <h3>Contents</h3>
+          <li>Overview</li>
+          <li>Highlights</li>
+          <li>Review</li>
+        </ul>
+      </div>
     </div>
   );
 }
