3d3577829c45a93f71f4ff05a56e5de7d1526650
emanuelpps <emanuelpages.ps@gmail.com>
2023-12-21 17:25:34 -0300

    added grid in project page and more info in project json

--- app/work/[projectName]/page.jsx
+++ app/work/[projectName]/page.jsx
@@ -21,60 +21,88 @@ function ProjectName({ params }) {
   console.log(projectPage);
 
   return (
-    <div className="pt-16 grid grid-cols-auto grid-rows-auto gap-4">
+    <div>
       {projectPage ? (
         <>
-          <div id="project-title-container" className="col-span-5">
-            <h1 className="text-center text-3xl">{projectPage.name}</h1>
-            <div id="h2-container" className="flex flex-row justify-center">
-              <h2 className="text-center ">{projectPage.class}</h2>
+          <div className="grid grid-cols-5 grid-rows-12 gap-4">
+            <div id="title-container" className="col-span-5">
+              <h1 className="text-center text-3xl">{projectPage.name}</h1>
+              <div id="h2-container" className="flex flex-row justify-center">
+                <h2 className="text-center ">{projectPage.class}</h2>
+              </div>
             </div>
-          </div>
-          <div className="row-span-6 col-start-1 row-start-2"></div>
-          <div
-            id="image-container"
-            className="flex col-span-3 row-span-4 col-start-2 row-start-2"
-          >
-            <Image
-              src={projectPage.img}
-              width={1200}
-              height={1200}
-              className="self-auto"
-              alt="Project Image"
-            />
-          </div>
-          <div
-            id="overview-container"
-            className="grid grid-cols-5 grid-rows-5 gap-4"
-          >
-            <div className="col-span-2 row-span-2">
-              <h4 className="flex flex-row">My Role -</h4>
-              <h3 className="flex flex-row">
-                {projectPage.infoProject[0].myRole}
-              </h3>
+            <div className="col-span-3 row-span-3 col-start-2 row-start-2">
+              <div
+                id="image-container"
+                className="flex col-span-3 row-span-4 col-start-2 row-start-2"
+              >
+                <Image
+                  src={projectPage.img}
+                  width={1200}
+                  height={1200}
+                  className="self-auto"
+                  alt="Project Image"
+                />
+              </div>
             </div>
-            <div className="col-span-2 row-span-2 col-start-1 row-start-4">
-              <h4>Team</h4>
-              <p>{projectPage.infoProject[0].team}</p>
+            <div
+              id="content-container"
+              className="row-span-3 col-start-5 row-start-2"
+            >
+              <ul>
+                <h3>Contents</h3>
+                <li>Overview</li>
+                <li>Highlights</li>
+                <li>Review</li>
+              </ul>
             </div>
-            <div className="col-span-2 row-span-5 col-start-4 row-start-1">
-              5
+            <div className="col-span-3 col-start-2 row-start-5">
+              <div
+                id="buttons-project-container"
+                className="flex justify-center mt-20 pb-20"
+              >
+                <button>code</button>
+                <button>github</button>
+              </div>
             </div>
-            <div className="col-span-2 col-start-1 row-start-3">6</div>
-            <div className="row-span-5 col-start-3 row-start-1">7</div>
+            <div className="col-span-2 row-span-2 col-start-1 row-start-6 pl-28">
+              <div id="role-container">
+                <h4>My Role</h4>
+                <h3>{projectPage.infoProject[0].myRole}</h3>
+              </div>
+            </div>
+            <div className="col-span-2 row-span-2 col-start-1 row-start-8 pl-28">
+              <div id="team-container">
+                {projectPage.infoProject[0].team ? <h4>Team</h4> : <></>}
+                {projectPage.infoProject[0].team?.map((teamMate) => (
+                  <p>{teamMate}</p>
+                ))}
+              </div>
+            </div>
+            <div className="col-span-2 row-span-4 col-start-4 row-start-6 grid grid-cols-1 justify-center pl-10 pr-20">
+              <div id="overview-container">
+                <h3>Overview</h3>
+                <p>{projectPage.infoProject[0].overview}</p>
+              </div>
+              <div id="highlights-container" className="mt-20">
+                <h3>Highlights</h3>
+                <p>{projectPage.infoProject[0].highlight}</p>
+              </div>
+            </div>
+            <div className="col-span-5 row-span-3 col-start-1 row-start-10">
+              <div id="review-container">
+                <h3>Review</h3>
+                <div id="gallery-container"></div>
+              </div>
+            </div>
+            <div className="row-span-4 col-start-1 row-start-2"></div>
+            <div className="row-span-4 col-start-3 row-start-6 justify-center bg-white"></div>
+            <div className="col-start-5 row-start-5"></div>
           </div>
         </>
       ) : (
         <p className="text-center">Loading Project... Please Wait</p>
       )}
-      <div className="row-span-4 col-start-5 row-start-2">
-        <ul>
-          <h3>Contents</h3>
-          <li>Overview</li>
-          <li>Highlights</li>
-          <li>Review</li>
-        </ul>
-      </div>
     </div>
   );
 }
