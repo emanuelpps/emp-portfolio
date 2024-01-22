a146357ff9ae7fca5f810b3150ba305a17d56ec1
emanuelpps <emanuelpages.ps@gmail.com>
2024-01-22 10:35:45 -0300

    correction in next.config.js

--- next.config.js
+++ next.config.js
@@ -11,17 +11,17 @@ module.exports = {
       },
     ];
   },
-  themes: {
-    colors: {
-      transparent: "transparent",
-      current: "currentColor",
-      myBlack: "#333333",
-      gray: "#4C4C4C",
-      white: "#D0D0D0",
-    },
-  },
-  image: {
-    domains: ["i.ibb.co", "duodigitalmarketing.000webhostapp.com"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "i.ibb.co",
+      },
+      {
+        protocol: "https",
+        hostname: "duodigitalmarketing.000webhostapp.com",
+      },
+    ],
   },
   nextConfig,
 };
