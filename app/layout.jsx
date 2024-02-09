bf3347c4058203f069a743a076b7c9b1c527d87c
emanuelpps <emanuelpages.ps@gmail.com>
2024-02-09 13:49:39 -0300

    added Goolge analytics tracking

--- app/layout.jsx
+++ app/layout.jsx
@@ -2,12 +2,13 @@ import "./globals.css";
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import { ToastContainer } from "react-toastify";
+import GoogleAnalytics from "./GoogleAnalytics";
 
 export const metadata = {
   title: "Emanuel Pagés | Frontend Developer",
   description:
     "I'm passionate about programming. I'm always seeking opportunities to enhance my skills and knowledge.",
-    lang: "en"
+  lang: "en",
 };
 const poppins = Inter({
   subsets: ["latin"],
@@ -20,6 +21,7 @@ export default async function RootLayout({ children }) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </Head>
       <body className="overflow-x-hidden scroll-smooth">
+        <GoogleAnalytics />
         {children}
         <ToastContainer closeButton={false} />
       </body>
