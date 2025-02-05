8b612d05f95211cdcc2a6018fe0d8233ad48864c
emanuelpps <emanuelpages.ps@gmail.com>
2025-02-05 11:25:57 -0300

    fix: next update

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,5 @@
 import "./globals.css";
 import { Outfit } from "next/font/google";
-import Head from "next/head";
 import "react-toastify/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/ReactToastify.css";
@@ -11,6 +10,10 @@ export const metadata = {
   description:
     "I'm passionate about programming. I'm always seeking opportunities to enhance my skills and knowledge.",
   lang: "en",
+  meta: {
+    charset: "UTF-8",
+    viewport: "width=device-width, initial-scale=1.0",
+  },
 };
 const outfit = Outfit({
   subsets: ["latin"],
@@ -19,9 +22,6 @@ const outfit = Outfit({
 export default async function RootLayout({ children }) {
   return (
     <html lang="en" className={`${outfit.className} scroll-smooth`}>
-      <Head>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      </Head>
       <body className="scroll-smooth">
         <GoogleAnalytics />
         {children}
